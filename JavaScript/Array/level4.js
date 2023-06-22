732f2993e4f67a6aa545710de66eaae06ce6e9a8
13Rai <bhabosonrai@gmail.com>
2023-06-22 09:30:10 +0545

    Updated code Q2

--- JavaScript/Array/level4.js
+++ JavaScript/Array/level4.js
@@ -6,16 +6,7 @@
 const name= 'hari'
 const allUsers = ['hari','shyam','thakur']
 
-/*
-for i < len(allUsers)
- if allUsers[i] = "hari"
- print true
- elif 
- false
-*/
-
 let i = 0
-
 
 let found = false;
 for (let i = 0; i <= allUsers.length, i++;); {
@@ -23,3 +14,19 @@ for (let i = 0; i <= allUsers.length, i++;); {
     found = true;
 }
 console.log(found)
+
+
+//Q2 find number of duplicates
+//expected output is : 2
+//true
+
+const user= 'hari'
+const userArr = ['hari','shyam','hari','thakur']
+
+let c = 0
+let dupes = 0
+for (let c = 0; c <= userArr.length, i++;);{
+    if (userArr[c] == user);
+    dupes +=1
+}
+console.log(dupes)
